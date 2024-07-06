9b8a24bbd720cc5d602bdce305e918325ca7e2d0
Ann KangHo <102593699+gangho0922@users.noreply.github.com>
2024-07-06 17:30:10 +0900

    :recycle: StudentMainView 코드 수정
    StudentMainView 코드 수정

--- App/Sources/Presentation/Main/Source/Scene/StudentMainView.swift
+++ App/Sources/Presentation/Main/Source/Scene/StudentMainView.swift
@@ -1,9 +1,11 @@
 import SwiftUI
 
 struct StudentMainView: View {
+    @StateObject var studentMainContainer: MVIContainer<StudentMainIntent, StudentMainModelStateProtocol>
+    
     @State private var currentIndex = 0
     let timer = Timer.publish(every: 3, on: .main, in: .common).autoconnect()
-    
+
     var body: some View {
         NavigationView {
             VStack {
@@ -37,14 +39,112 @@ struct StudentMainView: View {
                     .padding(.top, 32)
                     .padding(.leading, 16)
                 
-                MainMissionScrollView()
-                    .padding(.top, 8)
-                    .padding(.horizontal, 16)
+                VStack {
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 16) {
+                            ForEach(1..<7) {_ in
+                                VStack {
+                                    Text(studentMainContainer.model.tester)
+                                        .skFont(.pm14)
+                                        .multilineTextAlignment(.center)
+                                        .padding(.top, 16)
+                                    
+                                    Text(studentMainContainer.model.subtitle)
+                                        .skFont(.pr12)
+                                        .multilineTextAlignment(.center)
+                                        .lineLimit(3)
+                                        .padding(.top, 16)
+                                        .padding(.horizontal, 16)
+                                    
+                                    Spacer()
+                                    
+                                    Text(studentMainContainer.model.price)
+                                        .skFont(.pm16)
+                                        .multilineTextAlignment(.center)
+                                        .padding(.bottom, 24)
+                                }
+                                .background(Color.white)
+                                .frame(width: 117, height: 158)
+                                .cornerRadius(10)
+                            }
+                        }
+                        
+                    }
+                    .padding(.vertical, 16)
+                    .padding(.horizontal, 15.44)
+                    .background(Color.SKColorSystem.Gray.lightgray1.color)
+                    .cornerRadius(20)
+                }
+                .padding(.top, 8)
+                .padding(.horizontal, 16)
                 
-                MainRankingView()
-                    .padding(.top, 20)
+                VStack {
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 16) {
+                            ForEach(1..<4) { ranking in
+                                ZStack(alignment: .topLeading) {
+                                    VStack {
+                                        Image(.bannerImage3)
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fill)
+                                            .frame(width: 60, height: 60)
+                                            .clipShape(Circle())
+                                            .clipped()
+                                        
+                                        Text(studentMainContainer.model.profileName)
+                                            .skFont(.pm14)
+                                            .lineLimit(nil)
+                                            .fixedSize(horizontal: false, vertical: true)
+                                        
+                                        Spacer().frame(height: 16)
+                                        
+                                        Text(studentMainContainer.model.skPoint)
+                                            .skFont(.pm16)
+                                    }
+                                    .padding(.init(top: 18, leading: 25, bottom: 18, trailing: 25))
+                                    .background(Color.white)
+                                    .frame(height: 158)
+                                    .cornerRadius(10)
+                                    
+                                    if ranking == 1 {
+                                        Circle()
+                                            .frame(width: 20, height: 20)
+                                            .foregroundColor(Color.SKColorSystem.Medal.firstplace.color)
+                                            .overlay(
+                                                Text("1")
+                                                    .skFont(.pm14)
+                                            )
+                                    }
+                                    else if ranking == 2 {
+                                        Circle()
+                                            .frame(width: 20, height: 20)
+                                            .foregroundColor(Color.SKColorSystem.Medal.secondplace.color)
+                                            .overlay(
+                                                Text("2")
+                                                    .skFont(.pm14)
+                                            )
+                                    }
+                                    else if ranking == 3 {
+                                        Circle()
+                                            .frame(width: 20, height: 20)
+                                            .foregroundColor(Color.SKColorSystem.Medal.thirdplace.color)
+                                            .overlay(
+                                                Text("3")
+                                                    .skFont(.pm14)
+                                            )
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    .padding(.vertical, 16)
                     .padding(.horizontal, 16)
-    
+                    .background(Color.SKColorSystem.Gray.lightgray1.color)
+                    .cornerRadius(20)
+                }
+                .padding(.top, 20)
+                .padding(.horizontal, 16)
+                
             }
             .logoNavigationBar()
         }
