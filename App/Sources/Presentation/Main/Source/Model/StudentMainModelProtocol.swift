59b3cc642de3a9b651e522303c81a8ccdf48b0db
Ann KangHo <102593699+gangho0922@users.noreply.github.com>
2024-07-12 10:36:02 +0900

    :fire: Main관련 파일 삭제
    Main관련 파일 삭제

--- App/Sources/Presentation/Main/Source/Model/StudentMainModelProtocol.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-protocol StudentMainModelStateProtocol {
-    var tester: String { get }
-    var subtitle: String { get }
-    var price: String { get }
-    var profileName: String { get }
-    var skPoint: String { get }
-}
