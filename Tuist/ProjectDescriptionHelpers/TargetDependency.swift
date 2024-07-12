0395bda2f41ff0ae2500f793306ff83b81f6c8e8
Ann KangHo <102593699+gangho0922@users.noreply.github.com>
2024-07-12 10:34:38 +0900

    :sparkles: AppComponent+Order
    AppComponent+Order

--- Tuist/ProjectDescriptionHelpers/TargetDependency.swift
+++ Tuist/ProjectDescriptionHelpers/TargetDependency.swift
@@ -7,6 +7,7 @@ public extension TargetDependency {
 public extension TargetDependency.SPM {
     static let NeedleFoundation = TargetDependency.external(name: "NeedleFoundation")
     static let Alamofire = TargetDependency.external(name: "Alamofire")
+    static let PopupView = TargetDependency.external(name: "PopupView")
 }
 
 public extension Package {}
