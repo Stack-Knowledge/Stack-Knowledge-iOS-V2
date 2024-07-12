96a67e26857689ac1e521b63bfe99ebeb65684ca
Ann KangHo <102593699+gangho0922@users.noreply.github.com>
2024-07-12 11:44:44 +0900

    :sparkles: StudentMainModelStateProtocol 작성
    StudentMainModelProtocol 작성

---
+++ App/Sources/Presentation/StudentMain/Source/Model/StudentMainModelStateProtocol.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+protocol StudentMainModelStateProtocol {
+    var tester: String { get }
+    var subtitle: String { get }
+    var price: String { get }
+    var profileImageURL: String { get }
+    var profileName: String { get }
+    var skPoint: String { get }
+    var listName: String { get }
+    var listRequestDate: String { get }
+}
