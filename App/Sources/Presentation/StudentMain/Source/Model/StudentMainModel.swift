29f1cb0b678ec96ea857b748ba191619c536eb0b
Ann KangHo <102593699+gangho0922@users.noreply.github.com>
2024-07-13 18:03:55 +0900

    :sparkles: StudentMainModel
    컨벤션에 맞게 수정

--- App/Sources/Presentation/StudentMain/Source/Model/StudentMainModel.swift
+++ App/Sources/Presentation/StudentMain/Source/Model/StudentMainModel.swift
@@ -4,7 +4,7 @@ final class StudentMainModel: ObservableObject, StudentMainModelStateProtocol {
     @Published var tester: String = ""
     @Published var subtitle: String = ""
     @Published var price: String = ""
-    @Published var profileImageURL: String = ""
+    @Published var profileImageUrl: String = ""
     @Published var profileName: String = ""
     @Published var skPoint: String = ""
     @Published var listName: String = ""
