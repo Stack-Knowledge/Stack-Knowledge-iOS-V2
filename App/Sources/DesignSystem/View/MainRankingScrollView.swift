ceec4209a2d81d0563b278f2e91c786f698d50d5
Ann KangHo <102593699+gangho0922@users.noreply.github.com>
2024-07-16 20:01:43 +0900

    :sparkles: MainMissionScrollView UI 수정
    MainMissionScrollView UI 수정

--- App/Sources/DesignSystem/View/MainRankingScrollView.swift
+++ App/Sources/DesignSystem/View/MainRankingScrollView.swift
@@ -1,70 +1,77 @@
+import Service
 import SwiftUI
 
 struct MainRankingScrollView: View {
-    var profileImageUrl: String
-    var profileName: String
-    var skPoint: String
+    let profileImageUrl: String
+    let profileName: String
+    let skPoint: String
+    
+    let rankingDataList: [PointRankingListEntity]
+    
+    @State private var rankingCount: Int = 0
     
     var body: some View {
-        VStack {
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: 16) {
-                    ForEach(1..<4) { ranking in
-                        ZStack(alignment: .topLeading) {
-                            VStack {
-                                AsyncImage(url: URL(string: profileImageUrl))
-                                    .frame(width: 60, height: 60)
-                                    .clipShape(Circle())
-                                    .clipped()
-                                
-                                Text(profileName)
-                                    .skFont(.pm14)
-                                
-                                Spacer().frame(height: 16)
-                                
-                                Text(skPoint)
-                                    .skFont(.pm16)
-                            }
-                            .padding(.init(top: 25, leading: 25, bottom: 25, trailing: 25))
-                            .background(Color.white)
-                            .cornerRadius(10)
-                            .frame(height: 158)
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 16) {
+                ForEach(rankingDataList, id: \.rankingID) { ranking in
+                    ZStack(alignment: .topLeading) {
+                        VStack {
+                            AsyncImage(url: URL(string: ranking.user.profileImage))
+                                .frame(width: 60, height: 60)
+                                .clipShape(Circle())
+                                .clipped()
                             
-                            if ranking == 1 {
-                                Circle()
-                                    .frame(width: 20, height: 20)
-                                    .foregroundColor(Color.SKColorSystem.Medal.firstplace.color)
-                                    .overlay(
-                                        Text("1")
-                                            .skFont(.pm14)
-                                    )
-                            } 
-                            else if ranking == 2 {
-                                Circle()
-                                    .frame(width: 20, height: 20)
-                                    .foregroundColor(Color.SKColorSystem.Medal.secondplace.color)
-                                    .overlay(
-                                        Text("2")
-                                            .skFont(.pm14)
-                                    )
-                            } 
-                            else if ranking == 3 {
-                                Circle()
-                                    .frame(width: 20, height: 20)
-                                    .foregroundColor(Color.SKColorSystem.Medal.thirdplace.color)
-                                    .overlay(
-                                        Text("3")
-                                            .skFont(.pm14)
-                                    )
-                            }
+                            Text(ranking.user.userName)
+                                .skFont(.pm14)
+                            
+                            Spacer().frame(height: 16)
+                            
+                            Text(ranking.cumulatePoint.description)
+                                .skFont(.pm16)
                         }
+                        .padding(.init(top: 18, leading: 30, bottom: 18, trailing: 30))
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        
+                        if rankingCount == 0 {
+                            Circle()
+                                .frame(width: 20, height: 20)
+                                .foregroundColor(Color.SKColorSystem.Medal.firstplace.color)
+                                .overlay(
+                                    Text("1")
+                                        .skFont(.pm14)
+                                )
+                        } else if rankingCount == 1 {
+                            Circle()
+                                .frame(width: 20, height: 20)
+                                .foregroundColor(Color.SKColorSystem.Medal.secondplace.color)
+                                .overlay(
+                                    Text("2")
+                                        .skFont(.pm14)
+                                )
+                        } else if rankingCount == 2 {
+                            Circle()
+                                .frame(width: 20, height: 20)
+                                .foregroundColor(Color.SKColorSystem.Medal.thirdplace.color)
+                                .overlay(
+                                    Text("3")
+                                        .skFont(.pm14)
+                                )
+                        }
+                    }
+                    .onAppear {
+                        rankingCount += 1
                     }
                 }
             }
-            .padding(.vertical, 16)
-            .padding(.horizontal, 16)
-            .background(Color.SKColorSystem.Gray.lightgray1.color)
-            .cornerRadius(20)
+        }
+        .frame(height: 160)
+        .padding(.vertical, 16)
+        .padding(.horizontal, 16)
+        .background(Color.SKColorSystem.Gray.lightgray1.color)
+        .cornerRadius(20)
+        .onAppear {
+            rankingCount = 0
         }
     }
 }
