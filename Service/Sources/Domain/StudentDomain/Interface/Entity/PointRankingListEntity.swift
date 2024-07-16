c9705de0189ec4347e01eb7cac5a2b61adfc3b27
Ann KangHo <102593699+gangho0922@users.noreply.github.com>
2024-07-16 18:26:59 +0900

    :recycle: RaningID 추가
    rankingID 추가

--- Service/Sources/Domain/StudentDomain/Interface/Entity/PointRankingListEntity.swift
+++ Service/Sources/Domain/StudentDomain/Interface/Entity/PointRankingListEntity.swift
@@ -1,13 +1,16 @@
 import Foundation
 
 public struct PointRankingListEntity: Equatable {
+    public let rankingID: String
     public let cumulatePoint: Int
     public let user: UserInfoEntity
 
     public init(
+        rankingID: String,
         cumulatePoint: Int,
         user: UserInfoEntity
     ) {
+        self.rankingID = rankingID
         self.cumulatePoint = cumulatePoint
         self.user = user
     }
