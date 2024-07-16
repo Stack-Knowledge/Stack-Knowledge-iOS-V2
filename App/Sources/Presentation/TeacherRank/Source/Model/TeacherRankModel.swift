503e03151b3c2ac91dbe67ef6246b38258df0603
uuuunseo <jdbstjoo@gmail.com>
2024-07-16 23:00:34 +0900

    :recycle: :: TeacherRank / 오타수정

--- App/Sources/Presentation/TeacherRank/Source/Model/TeacherRankModel.swift
+++ App/Sources/Presentation/TeacherRank/Source/Model/TeacherRankModel.swift
@@ -3,11 +3,11 @@ import Foundation
 import Service
 
 final class TeacherRankModel: ObservableObject, TeacherRankModelStateProtocol {
-    var rankingList: [PointRankingListEntity] = [.init(rankingID: "1", cumulatePoint: 1000, user: .init(userId: "0115", userName: "이주연", profileImage: "https://thumb.pann.com/tc_480/http://fimg5.pann.com/new/download.jsp?FileID=64425399"))]
+    @Published var rankingList: [PointRankingListEntity] = []
 }
 
 extension TeacherRankModel: TeacherRankModelActionsProtocol {
-    func updateRankingList(rankingList: [PointRankingListEntity]) {
-        self.rankingList = rankingList
+    func updateRankingList(list: [PointRankingListEntity]) {
+        self.rankingList = list
     }
 }
