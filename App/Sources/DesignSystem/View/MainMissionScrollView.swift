3243646ac18420059856d2620ae9378656ddf055
Ann KangHo <102593699+gangho0922@users.noreply.github.com>
2024-07-18 20:47:02 +0900

    :recycle: MainMissionScrollView 코드 단축
    MainMissionScrollView 코드 단축

--- App/Sources/DesignSystem/View/MainMissionScrollView.swift
+++ App/Sources/DesignSystem/View/MainMissionScrollView.swift
@@ -10,15 +10,13 @@ struct MainMissionScrollView: View {
                 HStack(spacing: 16) {
                     ForEach(missionDataList.indices, id: \.self) { mission in
                         if let missionList = missionDataList[safe: mission] {
-                            VStack {
+                            VStack(alignment: .center) {
                                 Text(missionList.user.userName)
                                     .skFont(.pm14)
-                                    .multilineTextAlignment(.center)
                                     .padding(.top, 16)
                                 
                                 Text(missionList.missionTitle)
                                     .skFont(.pr12)
-                                    .multilineTextAlignment(.center)
                                     .lineLimit(3)
                                     .padding(.top, 16)
                                     .padding(.horizontal, 16)
@@ -27,7 +25,6 @@ struct MainMissionScrollView: View {
                                 
                                 Text(missionList.missionPoint.description)
                                     .skFont(.pm16)
-                                    .multilineTextAlignment(.center)
                                     .padding(.bottom, 24)
                             }
                             .padding(.init(top: 18, leading: 40, bottom: 18, trailing: 40))
