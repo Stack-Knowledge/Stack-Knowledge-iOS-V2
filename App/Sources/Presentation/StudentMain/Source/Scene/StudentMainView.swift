c86cc070ba54e958bde526a898fe01d5f9ecd9aa
Ann KangHo <102593699+gangho0922@users.noreply.github.com>
2024-07-18 20:59:52 +0900

    :recycle: StudentMainView 필요없는 코드 삭제
    StudentMainView 필요없는 코드 삭제

--- App/Sources/Presentation/StudentMain/Source/Scene/StudentMainView.swift
+++ App/Sources/Presentation/StudentMain/Source/Scene/StudentMainView.swift
@@ -5,8 +5,6 @@ import PopupView
 struct StudentMainView: View {
     @StateObject var studentMainContainer: MVIContainer<StudentMainIntent, StudentMainModelStateProtocol>
     
-    @State private var showPopup = false
-    
     var body: some View {
         NavigationView {
             ScrollView {
@@ -15,11 +13,9 @@ struct StudentMainView: View {
                 VStack(alignment: .leading) {
                     Text("* 문제는 12:30 ~ 19:30분까지 풀수 있습니다.")
                         .skFont(.pr12)
-                        .frame(maxWidth: .infinity, alignment: .leading)
                     
                     Text("문제 풀기")
                         .skFont(.ps18)
-                        .frame(maxWidth: .infinity, alignment: .leading)
                         .padding(.top, 15)
                     
                     MainMissionScrollView(missionDataList: studentMainContainer.model.missionDataList)
