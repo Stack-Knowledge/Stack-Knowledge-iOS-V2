08144e9b0f6488f514539e718983882fa12dccd5
Ann KangHo <102593699+gangho0922@users.noreply.github.com>
2024-07-18 16:03:34 +0900

    :sparkles: StudentMainModelStateProtocol 수정
    StudentMainModelStateProtocol 수정

--- App/Sources/Presentation/StudentMain/Source/Model/StudentMainModelStateProtocol.swift
+++ App/Sources/Presentation/StudentMain/Source/Model/StudentMainModelStateProtocol.swift
@@ -4,12 +4,4 @@ import Service
 protocol StudentMainModelStateProtocol {
     var missionDataList: [MissionListEntity] { get }
     var rankingDataList: [PointRankingListEntity] { get }
-    var tester: String { get }
-    var subtitle: String { get }
-    var price: String { get }
-    var profileImageUrl: String { get }
-    var profileName: String { get }
-    var skPoint: String { get }
-    var listName: String { get }
-    var listRequestDate: String { get }
 }
