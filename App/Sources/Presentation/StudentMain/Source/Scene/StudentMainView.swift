70451e49c02ff368968affd186973747ac3528a9
Ann KangHo <102593699+gangho0922@users.noreply.github.com>
2024-07-18 17:18:24 +0900

    :recycle: StudentMainView 코드 수정
    StudentMainView 코드 수정

--- App/Sources/Presentation/StudentMain/Source/Scene/StudentMainView.swift
+++ App/Sources/Presentation/StudentMain/Source/Scene/StudentMainView.swift
@@ -9,26 +9,25 @@ struct StudentMainView: View {
     
     var body: some View {
         NavigationView {
-            VStack {
+            ScrollView {
                 BannerView()
                 
-                Text("* 문제는 12:30 ~ 19:30분까지 풀수 있습니다.")
-                    .skFont(.pr12)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.leading, 16)
-                
-                Text("문제 풀기")
-                    .skFont(.ps18)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.top, 15)
-                    .padding(.leading, 16)
-                
-                MainMissionScrollView(missionDataList: studentMainContainer.model.missionDataList)
-                .padding(.top, 8)
-                .padding(.horizontal, 16)
-                
-                MainRankingScrollView(rankingDataList: studentMainContainer.model.rankingDataList)
-                .padding(.top, 10)
+                VStack(alignment: .leading) {
+                    Text("* 문제는 12:30 ~ 19:30분까지 풀수 있습니다.")
+                        .skFont(.pr12)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    Text("문제 풀기")
+                        .skFont(.ps18)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.top, 15)
+                    
+                    MainMissionScrollView(missionDataList: studentMainContainer.model.missionDataList)
+                        .padding(.top, 8)
+                    
+                    MainRankingScrollView(rankingDataList: studentMainContainer.model.rankingDataList)
+                        .padding(.top, 10)
+                }
                 .padding(.horizontal, 16)
             }
             .logoNavigationBar()
