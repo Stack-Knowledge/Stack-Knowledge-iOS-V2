5ec53fe940f0c2d2f7a52292476d453d759ef69e
Ann KangHo <102593699+gangho0922@users.noreply.github.com>
2024-07-18 16:02:58 +0900

    :sparkles: StudentMainModel 수정
    StudentMainModel 수정

--- App/Sources/Presentation/StudentMain/Source/Model/StudentMainModel.swift
+++ App/Sources/Presentation/StudentMain/Source/Model/StudentMainModel.swift
@@ -4,12 +4,4 @@ import Service
 final class StudentMainModel: ObservableObject, StudentMainModelStateProtocol {
     @Published var missionDataList: [MissionListEntity] = []
     @Published var rankingDataList: [PointRankingListEntity] = []
-    @Published var tester: String = ""
-    @Published var subtitle: String = ""
-    @Published var price: String = ""
-    @Published var profileImageUrl: String = ""
-    @Published var profileName: String = ""
-    @Published var skPoint: String = ""
-    @Published var listName: String = ""
-    @Published var listRequestDate: String = ""
 }
